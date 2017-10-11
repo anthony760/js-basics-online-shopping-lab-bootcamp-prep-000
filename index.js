e4b72fc5c9806fbaf568605e1a743b59cf141db6
Anthony Lopez <anthony760@gmail.com>
2017-10-11 05:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,28 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  var items = new Array();
+  
+  for (var key in cart) {
+    if (cart.hasOwnProperty(key)) {
+      items.push(${key} + " at $" + cart[key]);
+    }
+  }
+
+  var numberOfItems = items.length;
+
+  if (numberOfItems < 1) {
+    console.log(`Your shopping cart is empty.`);
+  } else if (numberOfItems === 1) {
+    console.log(`In your cart, you have ${items[0]}.`);
+  } else {
+    console.log(`In your cart, you have `);
+    for (let i = 0; i < numberOfItems - 1; i++) {
+      console.log(`${items[i]}, `);
+    }
+
+    console.log(`and ${items[numberOfItems - 1]}.`);
+  }
 }
 
 function total() {
