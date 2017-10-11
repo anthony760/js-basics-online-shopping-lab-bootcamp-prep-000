2cf71aece90d09d7ac9b192b9b3f0c88a7838c15
Anthony Lopez <anthony760@gmail.com>
2017-10-11 08:36:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,5 +77,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === "" || cardNumber === NaN) {
+    console.log(`Sorry, we don't have a credit card on file for you.`);
+  }
 }
